2eb89f84f9684a678c92dab258f97ba686d84f01
Mateus Nogueira de Oliveira <mateus.oliveira@fpf.br>
2018-08-16 16:40:23 -0400

    Layout inicial

--- ranking-pescaria/src/app/app.module.ts
+++ ranking-pescaria/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RankingComponent } from './ranking/ranking.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RankingComponent
   ],
   imports: [
     BrowserModule
